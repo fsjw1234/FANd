ef0ba508c2bfa597564c7f5d927c5ef9ad76ef2d
fsjw1234 <793139278@qq.com>
2017-04-09 00:02:46 +0800

    Signed-off-by: fsjw1234 <793139278@qq.com>

---
+++ src/pers/fand/test/Jjcc.java
@@ -0,0 +1,11 @@
+package pers.fand.test;
+
+public class Jjcc {
+	public static void main(String args[]){
+int a=4,b=2;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+	}
+}
---
+++ src/pers/fand/test/StudyJava.java
@@ -0,0 +1,11 @@
+package pers.fand.test;
+
+public class StudyJava {
+	
+	public static void main(String args[]){
+		System.out.println("<JAVA入门到精通>");
+		System.out.println("是我学习java语言的最佳书籍");
+		
+	}
+
+}
